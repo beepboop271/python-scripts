3fe30d880b5eed9e579c254815a86fc868f32769
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2020-06-10 02:44:40 -0400

    add some *totally ancient* scripts from like a year ago

---
+++ google-basketball-scores.py
@@ -0,0 +1,47 @@
+# Sketchy script to pull basketball scores
+# from the google thingy for the 2019
+# nba finals, raptors v warriors
+
+import urllib2
+import re
+import time
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
+url ="https://www.google.com/search?q=raptors+game&rlz=1C1CHBF_enCA722CA722&oq=raptors+game&aqs=chrome..69i57j69i60l5.1126j0j7&sourceid=chrome&ie=UTF-8#sie=m;/g/11h1m4tzw3;3;/m/05jvx;dt;fp;1;;"
+
+request = urllib2.Request(url, headers=headers)
+response = urllib2.urlopen(request)
+page = response.read()
+
+i = page.index("imso_mh__l-tm-sc imso_mh__scr-it")
+s = page[i+20:i+200]
+match = re.match(r"[^0-9]+([0-9]+)[^0-9]+([0-9]+)[^0-9]+", s)
+g, r = map(int, match.group(1, 2))
+print "g  r"
+print g, r
+gl = g
+rl = r
+
+while True:
+    request = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(request)
+    page = response.read()
+    try:
+        i = page.index("imso_mh__l-tm-sc imso_mh__scr-it")
+        s = page[i+20:i+200]
+        match = re.match(r"[^0-9]+([0-9]+)[^0-9]+([0-9]+)[^0-9]+", s)
+        g, r = map(int, match.group(1, 2))
+        if g-gl > 0:
+            print g, r
+            if g-gl == 3:
+                print "unepic gamer moment"
+        if r-rl > 0:
+            print g, r
+            if r-rl == 3:
+                print "epic gamer moment"
+        gl = g
+        rl = r
+    except(ValueError):
+        print "error"
+
+    time.sleep(1)
---
+++ horses-in-the-back.py
@@ -0,0 +1,31 @@
+# meme script to download pictures of horses off
+# google images and put it in a folder called the back
+# so that you can have the horses in the back
+
+import urllib
+import urllib2
+import re
+import ssl
+
+headers = {"User-Agent": "Mozilla/5.0"}
+url = "https://www.google.com/search?q=horses&tbm=isch&safe=active"
+
+request = urllib2.Request(url, headers=headers)
+response = urllib2.urlopen(request)
+page = response.read()
+request = None
+response = None
+
+sslthing = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+
+expr = re.compile(r"\"http([^\"]+)(\.jpg|\.png)")
+matches = re.findall(expr, page)
+
+for i in range(len(matches)):
+    matches[i] = "http"+matches[i][0]+matches[i][1]
+
+print "oh boy", len(matches)
+for i in range(len(matches)):
+    if i % 100 == 0:
+        print i
+    urllib.urlretrieve(matches[i], "the back/"+str(i)+matches[i][-4:], context=sslthing)
